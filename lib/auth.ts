8970c74621114b8ee8467dbed0ec677fd32c7e44
x1yizhuo <1yizhuo1@proton.me>
2023-10-29 01:50:38 -0500

    update fetch import

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,9 @@
-import fetch from "node-fetch";
 import { urls } from ".";
 import { User } from "./types";
 import { cast } from "./util";
 
 export const getFwUser = async (cookie: string): Promise<User> => {
+  const fetch = (await import("node-fetch")).default;
   const res = await fetch(urls.fw + "/api/users/@me", {
     headers: {
       authorization: cookie,
